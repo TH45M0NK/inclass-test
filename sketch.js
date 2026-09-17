6ca276d7e5e1244af5765b43fa99ad4aaa0d3c67
TH45M0NK <t.h.45.m.0.n.k@hotmail.com>
2026-09-16 18:42:42 -0700

    4th Updated - Fun Canvas

--- sketch.js
+++ sketch.js
@@ -1,1 +1,10 @@
 console.log("I believe I can do this!");
+
+function setup() {
+  createCanvas(800, 600);
+}
+function draw() {
+  background(20);
+  fill(mouseX, mouseY, 150);
+  circle(mouseX, mouseY, 20);
+}
